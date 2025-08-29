2ff75994c8b131ee1090d59209855c3fdcc56154
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-28 21:55:17 -0500

    feat: initial commit for src/flower_identifier.py

---
+++ src/flower_identifier.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+class FlowerIdentifier:
+    def __init__(self):
+        pass
+
+    def identify_flower(self, image):
+        # Logik zur Blumenidentifikation
+        return 'Identifizierte Blume'
