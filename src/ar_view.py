0ebae52e65b7fcd0d246b336d6a0eea73eed741d
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-28 21:55:14 -0500

    feat: initial commit for src/ar_view.py

---
+++ src/ar_view.py
@@ -0,0 +1,9 @@
+import cv2
+
+class ARView:
+    def __init__(self):
+        pass
+
+    def display_flower_info(self, flower):
+        # Logik zur Anzeige der Blume Informationen in der AR
+        print(f'Displaying information for {flower}')
